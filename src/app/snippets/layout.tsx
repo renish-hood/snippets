4d93a8e08807d51c712e0f5c4e59f26c7996cd83
renish9923@gmail.com <renish9923@gmail.com>
2025-06-01 13:06:44 +0530

    fix(ui): updated layout

--- src/app/snippets/layout.tsx
+++ src/app/snippets/layout.tsx
@@ -1,17 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-export default function SnippetsLayout({
-  children,
-  modal,
-}: {
-  children: ReactNode;
-  modal: ReactNode;
-}) {
-  return (
-    <>
-      {children}
-      {modal}
-      <div id="modal-root" />
-    </>
-  );
+export default function SnippetsLayout({ children }: { children: ReactNode }) {
+  return <div className="relative">{children}</div>;
 }
